59993397aa15d7aa4172295e26c79cd9b011dde2
devmahin <mahinhowlader123@gmail.com>
2024-07-12 02:41:44 +0600

    footer section completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../Component/HomeCompopnent/Banner";
 import Homesectionman from "../../Component/HomeCompopnent/Homesectionman";
+import Footer from "../Shered/Footer";
 import Navbar from "../Shered/Navbar";
 
 function Home() {
@@ -7,14 +8,15 @@ function Home() {
         <div>
             <Navbar></Navbar>
             <div className="container mx-auto">
-            <Banner></Banner>
-            <div className="flex flex-col gap-y-20 mt-20 px-10 md:px-14 lg:px-10">
-            <Homesectionman></Homesectionman>
-            <Homesectionman></Homesectionman>
-            <Homesectionman></Homesectionman>
-            <Homesectionman></Homesectionman>
+                <Banner></Banner>
+                <div className="flex flex-col gap-y-20 mt-20 px-10 md:px-14 lg:px-10">
+                    <Homesectionman></Homesectionman>
+                    <Homesectionman></Homesectionman>
+                    <Homesectionman></Homesectionman>
+                    <Homesectionman></Homesectionman>
+                </div>
             </div>
-            </div>
+            <Footer></Footer>
         </div>
     );
 }
